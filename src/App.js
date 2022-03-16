c419b70a83695f6cbd605b79b48bb239936db9a7
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2022-03-16 21:17:00 +0530

    created header component with three icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Section1 from './components/Section1';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section1 />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import '../styles/dist/header.css'
+import cartIcon from '../images/shopping-cart.svg'
+import notificationIcon from '../images/bell.svg'
+import navbarIcon from '../images/menu.svg'
+
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className="header">Header</div>
+    <div className="header">
+        <div className="header__navbarbtn">
+            <img className="header__icon" src={navbarIcon} />
+        </div>
+
+        <div className="header__notificationbtn">
+            <img className="header__icon" src={notificationIcon} />
+            <img className="header__icon" src={cartIcon} />
+        </div>
+    </div>
   )
 }
 

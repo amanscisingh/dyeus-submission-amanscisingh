1b247db417700094cd130bf139caaa41da93a185
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2022-03-17 02:11:49 +0530

    created section8 component comtaining 'with diana you are in control...' section

---
+++ src/components/Section8.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import '../styles/dist/section8.css'
+import '../styles/dist/global.css'
+
+const Section8 = () => {
+  return (
+    <div className="section8">
+        <br />
+        <br />
+        <h1 className="h1 section8__element">
+        With Diana, you’re in control of your health, your every day, and your story.
+        </h1>
+        <br />
+        <br />
+    </div>
+  )
+}
+
+export default Section8

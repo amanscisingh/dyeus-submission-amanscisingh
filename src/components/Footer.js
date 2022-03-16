b9239187f8277ed03470aa55ff0db4545a7a062f
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2022-03-17 03:11:55 +0530

    created footer component comtaining social links and other links

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import '../styles/dist/footer.css'
+import '../styles/dist/global.css'
+
+import facebookIcon from '../images/facebook.png'
+import twitterIcon from '../images/twitter.png'
+import rightArrow from '../images/right.svg'
+import mailIcon from '../images/mail.svg'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer__element footer__social">
+            <img className="footer__element__img" src={facebookIcon} alt="Facebook Page"  />
+            <img className="footer__element__img" src={twitterIcon} alt="Twitter Page" />
+        </div>
+
+        <div className="footer__element footer__routes">
+            <div className="footer__routes__element">
+                <p className="p footer__element">Products <img src={rightArrow} /> </p> 
+            </div>
+            <div className="footer__routes__element">
+                <p className="p footer__element">Our Science <img src={rightArrow} /> </p> 
+            </div>
+            <div className="footer__routes__element">
+                <p className="p footer__element">Vision & Mission <img src={rightArrow} /> </p> 
+            </div>
+            <div className="footer__routes__element">
+                <p className="p footer__element">About Us <img src={rightArrow} /> </p> 
+            </div>
+        </div>
+
+        <hr className="footer__element"/>
+
+        <p className="footer__element p small">Subscribe to our Newsletter</p>
+
+        <div className="footer__element footer__subscribe">
+            <input className="footer__subscribe__input" placeholder="Enter your email..." type="text" name="email" id="email" />
+            <button className="footer__subscribe__submitBtn" type="submit"> <img src={mailIcon} /> Activate</button>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
